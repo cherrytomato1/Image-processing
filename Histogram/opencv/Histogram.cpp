b020c782a12a3d589051640e591c179f41b516c3
cherrytomato1 <49678555+cherrytomato1@users.noreply.github.com>
2019-10-22 20:16:10 +0900

    tmpCDF 추가 -->cdf를 0~255의 값으로 지정

--- Histogram/opencv/Histogram.cpp
+++ Histogram/opencv/Histogram.cpp
@@ -29,7 +29,7 @@ Input 값을 역함수 취해 평평한 형태로 만둠.
 #define unsigned char uchar
 
 // 정수형 크기 설정 히스토그램(확률분포함수), CDF(누적분포함수), Equalization histogram
-int histogram[256], cdfOfHisto[256], histogramEqual[256];
+int histogram[256], cdfOfHisto[256], histogramEqual[256], tmpCDF[256];
 
 // 문자형 포인터 생성
 uchar** uc_alloc(int size_x, int size_y)
@@ -92,7 +92,7 @@ void write_ucmatrix(int size_x, int size_y, uchar** ucmatrix, char* filename)
 }
 
 // 히스토그램 이미지 출력(opencv 이미지로 출력)
-void get_hist1(uchar** img, int X_Size, int Y_Size)
+void get_hist1(uchar** img, int X_Size, int Y_Size, int mod)
 {
 	int i, j, tmp;
 	double tmp1;
@@ -136,45 +136,55 @@ void get_hist1(uchar** img, int X_Size, int Y_Size)
 	{
 		tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
 	}
-	//최대 값을 1로 하고 나머지 값들을 최대 값에 비례해서 크기를 만듬 0~1
+	//최대 값을 1로 하고 나머지 값들을 최대 값에 비례해서 크기를 만듬 0~255
+	printf("\n\nHisto %d \n\n\n", mod);
 	for (i = 0; i < 256; ++i)
 	{
 		tmp = (int)255 * (histogram[i] / tmp1);
 
 		cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
 	}
-
-	cvShowImage("Histo Equal ", imgHisto);         // 원본 히스토그램 출력
+	if(mod==0)
+		cvShowImage("Histo Line", imgHisto);
+	else
+		cvShowImage("Histo Equal ", imgHisto);         // 원본 히스토그램 출력
 
 
 	//모든 히스토그램을 더하여 다음 히스토그램
 	cdfOfHisto[0] = histogram[0];
+	printf("\n\ncdfHisto %d \n\n\n", mod);
 	for (i = 1; i < 256; i++)
 	{
 		cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
+		printf("%d, ", cdfOfHisto[i]);
 	}
 	// Draw the CDF of Histogram
 	//최대 히스토그램 값을(중첩된 마지막 값) t,p로 지정
 	tmp1 = (double)cdfOfHisto[255];
 	//그와 비례하게 나누어 값 입력(0~1)
+	printf("\n\ncdfHisto Temp%d \n\n\n", mod);
 	for (i = 0; i < 256; ++i)
 	{
 		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-
+		printf("%d ,", tmp);
 		cvLine(cdfImgHisto, cvPoint(i, 255), cvPoint(i, 256 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
+		tmpCDF[i] = tmp;
 	}
-
-	cvShowImage("CDF of Histogram ", cdfImgHisto); //누적 히스토그램
-
+	if(mod==0)
+		cvShowImage("Original CDF of Histogram", cdfImgHisto);
+	else
+		cvShowImage("CDF of Histogram ", cdfImgHisto); //누적 히스토그램
 	range = cdfOfHisto[255] - cdfOfHisto[0];
 	// printf("%d" %d \n", tp, range);
 
 	//range == 히스토그램의 범위 
+
+	printf("\n\nHistoEqual %d \n\n\n", mod);
 	histogramEqual[0] = 0;
 	for (i = 0; i < 256; ++i) {
 		t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
 		histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
-		
+		printf("%d, ", histogramEqual[i]);
 		//0~255사이로 값을 고정ㄴ
 	}
 	//히스토그램의 평활화ㄹㄴ 작업
@@ -182,172 +192,16 @@ void get_hist1(uchar** img, int X_Size, int Y_Size)
 
 	cvReleaseImage(&imgHisto);						//메모리 해제
 	cvReleaseImage(&cdfImgHisto);					//메모리 해제
+
+	/*
 	for (i = 0; i < Y_Size; ++i)
 		for (j = 0; j < X_Size; ++j)
 			img[i][j] = histogramEqual[img[i][j]];
-}
-
-void get_hist(uchar** img, int X_Size, int Y_Size)
-{
-	int i, j, tmp;
-	double tmp1;
-	int t, tp, range, hrange;
-	CvSize histoSize, cdfSize;
-	IplImage* imgHisto, * cdfImgHisto;
-
-	histoSize.width = 256;
-	histoSize.height = 256;
-
-	cdfSize.width = 256;
-	cdfSize.height = 256;
-
-	imgHisto = cvCreateImage(histoSize, 8, 1);
-	cdfImgHisto = cvCreateImage(cdfSize, 8, 1);
-
-	for (i = 0; i < histoSize.height; i++)
-		for (j = 0; j < histoSize.width; j++)
-		{
-			((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
-		}
-
-	for (i = 0; i < cdfSize.height; i++)
-		for (j = 0; j < cdfSize.width; j++)
-		{
-			((uchar*)(cdfImgHisto->imageData + cdfImgHisto->widthStep * i))[j] = 0;
-		}
-
-	tp = X_Size * Y_Size;
-
-	for (i = 0; i < 256; i++)
-		histogram[i] = 0;
-
-	for (i = 0; i < Y_Size; i++)
-		for (j = 0; j < X_Size; j++)
-			histogram[img[i][j]]++;
-	
-	tmp1 = 0;
-
-	for (i = 0; i < 256; ++i)
-	{
-		tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
-	}
-	for (i = 0; i < 256; ++i)
-	{
-		tmp = (int)255 * (histogram[i] / tmp1);
-		cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-	cvShowImage("Histo Line", imgHisto);         // 히스토그램 equasl
-	cdfOfHisto[0] = histogram[0];
-	for (i = 0; i < 256; i++)
-		cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
-
-	//Draw the CDF of Histogram
-	tmp1 = (double)cdfOfHisto[255];
-	for (i = 0; i < 256; ++i)
-	{
-		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-
-		cvLine(cdfImgHisto, cvPoint(i, 255), cvPoint(i, 256 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-	cvShowImage("Original CDF of Histogram", cdfImgHisto);
-
-	range = cdfOfHisto[255] - cdfOfHisto[0];
-	histogramEqual[0] = 0;
-	for (i = 1; i < 256; ++i) {
-		t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
-		histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
-	}
-	cvReleaseImage(&imgHisto);
-	for (i = 0; i < Y_Size; ++i)
-		for (j = 0; j < X_Size; ++j)
-			img[i][j] = histogramEqual[img[i][j]];
+			*/
 }
 
 
-void get_hist2(uchar** img, int X_Size, int Y_Size,int *tempHisto)
-{
-	int i, j, tmp;
-	double tmp1;
-	int t, tp, range, hrange;
-	CvSize histoSize, cdfSize;
-	IplImage* imgHisto, * cdfImgHisto;
 
-	histoSize.width = 256;
-	histoSize.height = 256;
-
-	cdfSize.width = 256;
-	cdfSize.height = 256;
-
-	imgHisto = cvCreateImage(histoSize, 8, 1);
-	cdfImgHisto = cvCreateImage(cdfSize, 8, 1);
-
-	for (i = 0; i < histoSize.height; i++)
-		for (j = 0; j < histoSize.width; j++)
-		{
-			((uchar*)(imgHisto->imageData + imgHisto->widthStep * i))[j] = 0;
-		}
-
-	for (i = 0; i < cdfSize.height; i++)
-		for (j = 0; j < cdfSize.width; j++)
-		{
-			((uchar*)(cdfImgHisto->imageData + cdfImgHisto->widthStep * i))[j] = 0;
-		}
-
-	tp = X_Size * Y_Size;
-
-	for (i = 0; i < 256; i++)
-		histogram[i] = 0;
-
-	for (i = 0; i < Y_Size; i++)
-		for (j = 0; j < X_Size; j++)
-			histogram[img[i][j]]++;
-
-	tmp1 = 0;
-
-	for (i = 0; i < 256; ++i)
-	{
-		tmp1 = tmp1 > histogram[i] ? tmp1 : histogram[i];
-	}
-	for (i = 0; i < 256; ++i)
-	{
-		tmp = (int)255 * (histogram[i] / tmp1);
-		cvLine(imgHisto, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-	cvShowImage("TestTest Histo Line", imgHisto);         // 히스토그램 equasl
-	cdfOfHisto[0] = histogram[0];
-	for (i = 0; i < 256; i++)
-		cdfOfHisto[i] = cdfOfHisto[i - 1] + histogram[i];
-
-	for (i = 0; i < 256; i++)
-		tempHisto[i] = cdfOfHisto[i];
-		//tempHisto[i] = histogram[i];
-
-	//Draw the CDF of Histogram
-	tmp1 = (double)cdfOfHisto[255];
-	for (i = 0; i < 256; ++i)
-	{
-		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-
-		cvLine(cdfImgHisto, cvPoint(i, 255), cvPoint(i, 256 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-	cvShowImage("TESt CDF of Histogram", cdfImgHisto);
-
-	range = cdfOfHisto[255] - cdfOfHisto[0];
-	histogramEqual[0] = 0;
-	for (i = 1; i < 256; ++i) {
-		t = (int)ceil(((cdfOfHisto[i] - cdfOfHisto[0]) * 255.0) / range);
-		histogramEqual[i] = (t < 0) ? 0 : (t > 255) ? 255 : t;
-	}
-	cvReleaseImage(&imgHisto);
-}
-
-/*
-
- 원하는 히스토그램을 입력해서 원본 이미지와 입력 히스토그램과 같게 만듬.
-
-
-
-*/
 void get_Match(uchar** img, int X_Size, int Y_Size, int histogramSpec[256], int histogramMatch[256])
 {
 	int i, j, tmp, matchz = 0;
@@ -359,12 +213,10 @@ void get_Match(uchar** img, int X_Size, int Y_Size, int histogramSpec[256], int 
 	{
 		histogramMatch[i] = 0;
 		for (j = 0; j < 256; j++)
-		{
 			if ((i - histogramSpec[j]) > 0)
 				histogramMatch[i] = j;
-		}
 	}
-	/*
+
 	for (i = 0; i < 256; ++i)
 	{
 		diff = abs(i - histogramSpec[0]);
@@ -379,7 +231,7 @@ void get_Match(uchar** img, int X_Size, int Y_Size, int histogramSpec[256], int 
 		}
 		histogramMatch[i] = (uchar)matchz;
 	}
-	*/
+
 	for (i = 0; i < Y_Size; ++i)
 		for (j = 0; j < X_Size; ++j)
 			img[i][j] = histogramMatch[img[i][j]];
@@ -389,37 +241,19 @@ void get_Match(uchar** img, int X_Size, int Y_Size, int histogramSpec[256], int 
 
 int main(int argc, char* argv[])
 {
-	int i, j, tmp;
-
-	double tmp1;
-
-	int tempHisto[256],tempHisto2[256],resHisto[256];
-
-	IplImage* cvImg,*testimg;
-	CvSize imgSize,imgSize2, histoSize;
-	uchar** img,**img2;
-	if (argc != 7)
+	int i, j;
+	IplImage* cvImg;
+	CvSize imgSize;
+	uchar** img;
+	if (argc != 4)
 	{
 		printf("arg error \n");
 		exit(0);
 	}
 	imgSize.width = atoi(argv[2]);
 	imgSize.height = atoi(argv[3]);
-
-	imgSize2.width = atoi(argv[5]);
-	imgSize2.height = atoi(argv[6]);
-
-
-
 	img = uc_alloc(imgSize.width, imgSize.height);
-
-	img2 = uc_alloc(imgSize2.width, imgSize2.height);
-
 	read_unmatrix(imgSize.width, imgSize.height, img, argv[1]);
-
-
-	read_unmatrix(imgSize2.width, imgSize2.height, img2, argv[4]);
-
 
 	cvImg = cvCreateImage(imgSize, 8, 1);
 	for (i = 0; i < imgSize.height; i++)
@@ -430,53 +264,17 @@ int main(int argc, char* argv[])
 	cvNamedWindow(argv[1], 1);
 
 	cvShowImage(argv[1], cvImg);
-	/*
-	get_hist(img, imgSize.width, imgSize.height);
-	for (i = 0; i < imgSize.height; i++)
-		for (j = 0; j < imgSize.width; j++)
-		{
-			((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
-		}
-	cvShowImage("Histogram Equalizer...", cvImg);
+	get_hist1(img, imgSize.width, imgSize.height,0);
 
-	*/
-	//get_hist1(img, imgSize.width, imgSize.height);
-
-	//get_hist1(img2, imgSize2.width, imgSize2.height);
-
-	//get_hist2(img, imgSize.width, imgSize.height, tempHisto);
-
-	get_hist2(img2, imgSize2.width, imgSize2.height, tempHisto2);
-
-	histoSize.width = 256;
-	histoSize.height = 256;
-
-	testimg = cvCreateImage(histoSize, 8, 1);
-
-	tmp1 = (double)cdfOfHisto[255];
-	for (i = 0; i < 256; ++i)
-	{
-		tmp = (int)255 * (cdfOfHisto[i] / tmp1);
-		cvLine(testimg, cvPoint(i, 255), cvPoint(i, 255 - tmp), CV_RGB(255, 255, 255), 1, 8, 0);
-	}
-	cvShowImage("TNEOEOE", testimg);         // 히스토그램 equasl
-	
-	get_Match(img, imgSize.width, imgSize.height, histogramEqual, tempHisto2);
-
+	get_Match(img, imgSize.width, imgSize.height, tmpCDF,tmpCDF);
 
 	for (i = 0; i < imgSize.height; i++)
 		for (j = 0; j < imgSize.width; j++)
 		{
 			((uchar*)(cvImg->imageData + cvImg->widthStep * i))[j] = img[i][j];
 		}
-		
-	cvShowImage("TestTest...", cvImg);
-
-	get_hist1(img, imgSize.width, imgSize.height);
-	
-	
-
-
+	cvShowImage("Histogram match...", cvImg);
+	get_hist1(img, imgSize.width, imgSize.height,1);
 	cvWaitKey(0);
 	cvDestroyWindow(argv[1]);
 	cvReleaseImage(&cvImg);
