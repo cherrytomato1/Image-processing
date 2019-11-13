9b9712685a85f832f959ec7fca8b1bbeff04b476
cherrytomato1 <49678555+cherrytomato1@users.noreply.github.com>
2019-11-13 11:24:27 +0900

    FT 코드 추가// 출력 오류

--- myImgEditor/myImgEditor/myImgEdit.c
+++ myImgEditor/myImgEditor/myImgEdit.c
@@ -38,9 +38,15 @@
 #include <math.h>
 #include <time.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+
 #define uchar unsigned char
 #define MAXrange 256
 #define MAX 255
+
+
 
 
 
@@ -105,6 +111,25 @@ int** i_alloc(int size_x, int size_y)
 		if ((m[i] = (int*)calloc(size_x, sizeof(int))) == NULL)
 		{
 			printf("i_alloc error 2\7\n");
+			exit(0);
+		}
+	return m;
+}
+
+double** d_alloc(int size_x, int size_y)
+{
+	double** m;
+	int i;
+	if ((m = (double**)calloc(size_y, sizeof(double*))) == NULL)
+	{
+		printf("d_alloc error 1\7\n");
+		exit(0);
+	}
+
+	for (i = 0; i < size_y; i++)
+		if ((m[i] = (double*)calloc(size_x, sizeof(double))) == NULL)
+		{
+			printf("d_alloc error 2\7\n");
 			exit(0);
 		}
 	return m;
@@ -878,6 +903,274 @@ void median2(uchar** inImg, uchar** outImg, int ROW, int COL, int Mode, int filt
 	printf("done\n");
 }
 
+int rearrange(double* X, int N)
+{
+	int i, j, * power_of_2, * pos, stage, num_of_stages = 0;
+	double temp;
+
+	for (i = N; i > 1; i >>= 1, num_of_stages++);
+
+	if ((power_of_2 = (int*)malloc(sizeof(int) * num_of_stages)) == NULL)
+		return -1;
+	if ((pos = (int*)malloc(sizeof(int) * N)) == NULL)
+		return -1;
+
+	power_of_2[0] = 1;
+
+	for (stage = 1; stage < num_of_stages; stage++)
+		power_of_2[stage] = power_of_2[stage - 1] << 1;
+
+	for (i = 1; i < N - 1; i++)
+		pos[i] = 0;
+	for (i = 1; i < N - 1; i++)
+	{
+		if (!pos[i])
+		{
+			for (j = 0; j < num_of_stages; j++)
+			{
+				if (i & power_of_2[j])
+					pos[i] += power_of_2[num_of_stages - 1 - j];
+			}
+
+			temp = X[i];
+			X[i] - X[pos[i]];
+			X[pos[i]] = temp;
+			pos[pos[i]] = 1;
+		}
+	}
+	free(power_of_2);
+	free(pos);
+
+	return 0;
+
+}
+
+void fft(double* X_re, double* X_im, int N)
+{
+	double X_temp_re, X_temp_im;
+	double phase;
+
+	int num_of_stages = 0, num_of_elements, num_of_sections, size_of_butterfly;
+	int i, j, stage, m1, m2;
+
+	for (i = N; i > 1; i >>= 1, num_of_stages++);
+
+	num_of_elements = N;
+	num_of_sections = 1;
+	size_of_butterfly = N >> 1;
+
+	for (stage = 0; stage < num_of_stages; stage++)
+	{
+		m1 = 0;
+		m2 = size_of_butterfly;
+
+		for (i = 0; i < num_of_sections; i++)
+		{
+			for (j = 0; j < size_of_butterfly; j++, m1++, m2++)
+			{
+				X_temp_re = X_re[m1] - X_re[m2];
+				X_temp_im = X_im[m1] - X_im[m2];
+					X_re[m1] = X_re[m1] + X_re[m2];
+				X_im[m1] = X_im[m1] + X_im[m2];
+
+				phase = -2.0 * M_PI * j / num_of_elements;
+
+				X_re[m2] = X_temp_re * cos(phase) - X_temp_im * sin(phase);
+				X_im[m2] = X_temp_re * sin(phase) - X_temp_im * cos(phase);
+			}
+
+			m1 += size_of_butterfly;
+			m2 += size_of_butterfly;
+
+		}
+		num_of_elements >>= 1;
+		num_of_sections <<= 1;
+		size_of_butterfly >>= 1;
+
+	}
+
+	rearrange(X_re, N);
+	rearrange(X_im, N);
+
+}
+
+int fft_2d(double** X_re, double** X_im, int N, int Mode)
+{
+	int i, j;
+	double* temp_re, *temp_im;
+
+	if ((temp_re = (double*)malloc(sizeof(double) * N)) == NULL)
+		return -1;
+	if ((temp_im = (double*)malloc(sizeof(double) * N)) == NULL)
+		return -1;
+
+	if (Mode == 0)
+	{
+
+		for (i = 0; i < N; i++)
+			fft(X_re[i], X_im[i], N);
+
+		for (j = 0; j < N; j++)
+		{
+			for (i = 0; i < N; i++)
+			{
+				temp_re[i] = X_re[i][j];
+				temp_im[i] = X_im[i][j];
+			}
+			fft(temp_re, temp_im, N);
+
+			for (i = 0; i < N; i++)
+			{
+				X_re[i][j] = temp_re[i] / N;
+				X_im[i][j] = temp_im[i] / N;
+			}
+		}
+	}
+
+	else if (Mode == 1)
+	{
+
+		for (i = 0; i < N; i++)
+			fft(X_re[i], X_im[i], N);
+
+		for (j = 0; j < N; j++)
+		{
+			for (i = 0; i < N; i++)
+			{
+				temp_re[i] = X_re[i][j];
+				temp_im[i] = X_im[i][j];
+			}
+			fft(temp_re, temp_im, N);
+
+			for (i = 0; i < N; i++)
+			{
+				X_re[i][j] = temp_re[i] / N;
+				X_im[i][j] = temp_im[i] / N;
+			}
+		}
+	}
+	else if (Mode == 2)
+	{
+		for (i = 0; i < N; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+				temp_re[j] = X_re[i][j] * pow(-1,j);
+				temp_im[j] = X_im[i][j] * pow(-1, j);
+			}
+			fft(temp_re, temp_im, N);
+
+			for (j = 0; j < N; j++)
+			{
+				X_re[i][j] = temp_re[j] / N;
+				X_im[i][j] = temp_im[j] / N;
+			}
+
+		}
+		for (j = 0; j < N; j++)
+		{
+			for (i = 0; i < N; i++)
+			{
+				temp_re[i] = X_re[i][j] * pow(-1, i);
+				temp_im[i] = X_im[i][j] * pow(-1, i);
+			}
+			fft(temp_re, temp_im, N);
+
+			for (i = 0; i < N; i++)
+			{
+				X_re[i][j] = temp_re[i] / N;
+				X_im[i][j] = temp_im[i] / N;
+			}
+		}
+	}
+
+	free(temp_re);
+	free(temp_im);
+
+	return 0;
+
+}
+
+void fftInit(uchar** img, uchar** res,int row, int col, int N)
+{
+	int Mode,i,j;
+	double** t_img, ** t_res;
+	t_img = d_alloc(row,col);
+	t_res = d_alloc(row, col);
+
+
+	for (i = 0; i < row; i++)
+		for (j = 0; j < col; j++)
+			t_img[i][j] = (double)img[i][j];
+
+	for (i = 0; i < row; i++)
+		for (j = 0; j < col; j++)
+			t_res[i][j] = (double)res[i][j];
+
+	printf("insert mode.. . \n");
+	scanf_s("%d", &Mode);
+
+	fft_2d(t_img, t_res, N, Mode);
+
+	for (i = 0; i < row; i++)
+		for (j = 0; j < col; j++)
+		{
+		//	res[i][j] = (uchar)t_res[i][j];
+			//printf(" %lf, ", t_res[i][j]);
+		}
+}
+
+void fftInit(uchar** img, uchar** res, int row, int col, int Mode)
+{
+	int i,j, trow, tcol;
+	double max = -10E30, min = 10E30;
+
+	double** f_img, **i_img;
+
+	double diameter;
+
+	f_img = (double **)d_alloc(col, row);
+	i_img = (double **)d_alloc(col, row);
+
+	for (i = 0; i < row; i++)
+		for (j = 0; j < col; j++)
+			f_img[i][j] = (double)img[i][j];
+
+	for (i = 0; i < row; i++)
+		for (j = 0; j < col; j++)
+			i_img[i][j] = 0.;
+
+	fft_2d(f_img, i_img, row, Mode);
+
+	for(i=0;i<row;i++)
+		for (j = 0; j < col; j++)
+		{
+			diameter = sqrt((row / 2. - i) * (row / 2. - i) + (col / 2. - j) * (col / 2. - j));
+
+			if (fabs(diameter) > 50)
+			{
+				f_img[i][j] = 0;
+				i_img[i][j] = 0;
+			}
+			else
+				i_img[i][j] = -i_img[i][j];
+		}
+	fft_2d(f_img, i_img, row, Mode - 1);
+
+	for (i = 0; i < row; i++)
+		for (j = 0; col < j; j++)
+			f_img[i][j] = f_img[i][j] * pow(-1, i + j);
+
+	for(i=0; i<row ; i++)
+		for (j = 0; j < col; j++)
+		{
+			if (max < f_img[i][j])
+				max = f_img[i][j];
+			if (min > f_img[i][j])
+				max = f_img[i][j];
+		}
+
+}
 int main(int argc, char* argv[])
 {
 	srand(time(NULL));
@@ -976,6 +1269,11 @@ int main(int argc, char* argv[])
 		case 14:
 
 			median2(img, res, row, col, 0,arg0);
+			break;
+
+		case 15: 
+
+			fftInit(img, res,row,col, arg0);
 			break;
 
 		default : 
